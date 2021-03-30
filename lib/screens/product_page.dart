8f172d093a6f375b692f06598b38a27aa344d93f
SPEAR22 <lokeshy142@gmail.com>
2021-03-30 18:52:50 +0530

    Day7

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:temp_h/constants.dart';
 import 'package:temp_h/widget/custom_action_bar.dart';
+import 'package:temp_h/widget/image_swipe.dart';
 
 class ProductPage extends StatefulWidget {
   final String productId;
@@ -11,7 +13,8 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductNameState extends State<ProductPage> {
-  final CollectionReference _productsRef = FirebaseFirestore.instance.collection("Products");
+  final CollectionReference _productsRef =
+      FirebaseFirestore.instance.collection("Products");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,38 +24,74 @@ class _ProductNameState extends State<ProductPage> {
             future: _productsRef.doc(widget.productId).get(),
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Text("Error: ${snapshot.error}"),
-              ),
-            );
-          }
-          if(snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> documentData = snapshot.data.data();
-            return ListView(
-              children: [
-                Image.network(
-                  "${documentData['images'][0]}"
-                ),
-                Text("Product Name"),
-                Text("Price"),
-                Text("Description"),
-                Text("Select Size"),
-              ],
-            );
-          }
+                return Scaffold(
+                  body: Center(
+                    child: Text("Error: ${snapshot.error}"),
+                  ),
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> documentData = snapshot.data.data();
+                List imageList = documentData['images'];
+                return ListView(
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                  children: [
+                    ImageSwipe(
+                      imageList: imageList,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 24.0, left: 24.0, right: 24.0, bottom: 4.0),
+                      child: Text(
+                        "${documentData["name"]}" ?? "Product Name",
+                        style: Constants.boldHeading,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 24.0),
+                      child: Text(
+                        "\$ ${documentData['price']}" ?? "Price",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Theme.of(context).accentColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 24.0),
+                      child: Text(
+                        "${documentData['desc']}" ?? "Description",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 24.0, horizontal: 24.0),
+                      child: Text(
+                        "Select Size",
+                        style: Constants.regularDarkText,
+                      ),
+                    ),
+                  ],
+                );
+              }
 
-          return Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
+              return Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
             },
           ),
           CustomActionBar(
             hasBackArrow: true,
             hasTitle: false,
-        
+            hasBackground: false,
           )
         ],
       ),
